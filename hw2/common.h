96cd865c58bfe83b1164171f87aa7a7f15afd4fa
Adrian Henle <henlee@br013.ib.bridges2.psc.edu>
2021-02-12 16:11:36 -0500

    formatting

--- hw2/common.h
+++ hw2/common.h
@@ -1,19 +1,22 @@
 #ifndef __CS267_COMMON_H__
 #define __CS267_COMMON_H__
 
-inline int min( int a, int b ) { return a < b ? a : b; }
-inline int max( int a, int b ) { return a > b ? a : b; }
+inline int min(int a, int b)
+{
+    return a < b ? a : b;
+}
 
-//
+inline int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
 //  saving parameters
-//
 const int NSTEPS = 1000;
 const int SAVEFREQ = 10;
 
-//
 // particle data structure
-//
-typedef struct 
+typedef struct
 {
   double x;
   double y;
@@ -26,28 +29,28 @@ typedef struct
 //
 //  timing routines
 //
-double read_timer( );
+double read_timer();
 
 //
 //  simulation routines
 //
-void set_size( int n );
-void init_particles( int n, particle_t *p );
-void apply_force( particle_t &particle, particle_t &neighbor , double *dmin, double *davg, int *navg);
-void move( particle_t &p );
+void set_size(int n);
+void init_particles(int n, particle_t *p);
+void apply_force(particle_t &particle, particle_t &neighbor, double *dmin, double *davg, int *navg);
+void move(particle_t &p);
 
 
 //
 //  I/O routines
 //
-FILE *open_save( char *filename, int n );
-void save( FILE *f, int n, particle_t *p );
+FILE *open_save(char *filename, int n);
+void save(FILE *f, int n, particle_t *p);
 
 //
 //  argument processing routines
 //
-int find_option( int argc, char **argv, const char *option );
-int read_int( int argc, char **argv, const char *option, int default_value );
-char *read_string( int argc, char **argv, const char *option, char *default_value );
+int find_option(int argc, char **argv, const char *option);
+int read_int(int argc, char **argv, const char *option, int default_value);
+char *read_string(int argc, char **argv, const char *option, char *default_value);
 
 #endif
